d10c9af9018ae7d1a7ad20972952a92b2a75bd95
Yamil Ayma <yamilaymadev@gmail.com>
2026-04-23 12:31:12 -0500

    feat (SEO) - Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
+  site: 'https://notebooklm-prompt-styles.vercel.app',
   output: 'static',
+  integrations: [sitemap()],
   vite: {
     plugins: [
       Icons({
